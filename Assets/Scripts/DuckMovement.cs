d0066900573f9f0a3ac0781d0bb5f28a208b2223
Emily Krohn <emilykrohn2@gmail.com>
2024-06-18 15:24:13 -0700

    Fix Duck starting X position
    Y position is starting in the same spot regardless of where the duck is in the scene before the game starts. Also added frequency and amplitude to adjust how much and how fast the duck moves up and down.

--- Assets/Scripts/DuckMovement.cs
+++ Assets/Scripts/DuckMovement.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class DuckMovement : MonoBehaviour
 {
     [SerializeField] float speed = 20f;
+    [SerializeField] float frequency = 1f;
+    [SerializeField] float amplitude = 1f;
     Vector3 previousPosition;
     Vector3 currentPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        previousPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentPosition = Vector3.up * Mathf.Sin(Time.time * 2);
+        currentPosition = Vector3.up * Mathf.Sin(Time.time * frequency) * amplitude;
         currentPosition += (previousPosition.x * Vector3.right) + Vector3.right * speed * Time.deltaTime;
         transform.position = currentPosition;
         previousPosition = currentPosition;
