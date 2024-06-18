c2105b5d7afec18d3959d7e1e97a0708f204fba4
Emily Krohn <emilykrohn2@gmail.com>
2024-06-18 15:40:26 -0700

    Fix Y Position Bug

--- Assets/Scripts/DuckMovement.cs
+++ Assets/Scripts/DuckMovement.cs
@@ -9,17 +9,19 @@ public class DuckMovement : MonoBehaviour
     [SerializeField] float amplitude = 1f;
     Vector3 previousPosition;
     Vector3 currentPosition;
+    Vector3 yOffset;
 
     // Start is called before the first frame update
     void Start()
     {
         previousPosition = transform.position;
+        yOffset = transform.position.y * Vector3.up;
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentPosition = Vector3.up * Mathf.Sin(Time.time * frequency) * amplitude;
+        currentPosition = yOffset + (Vector3.up * Mathf.Sin(Time.time * frequency) * amplitude);
         currentPosition += (previousPosition.x * Vector3.right) + Vector3.right * speed * Time.deltaTime;
         transform.position = currentPosition;
         previousPosition = currentPosition;
