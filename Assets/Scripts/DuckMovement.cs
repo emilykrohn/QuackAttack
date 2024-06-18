5b37d5a0d0e0168b9715a48abeb846bfa50e7240
Emily Krohn <emilykrohn2@gmail.com>
2024-06-18 15:13:43 -0700

    Duck Moves Right and Up and Down
    Bug where the duck always starts in the middle of the screen at x = 0 when it should start where it has been placed in the scene.

---
+++ Assets/Scripts/DuckMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 20f;
+    Vector3 previousPosition;
+    Vector3 currentPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPosition = Vector3.up * Mathf.Sin(Time.time * 2);
+        currentPosition += (previousPosition.x * Vector3.right) + Vector3.right * speed * Time.deltaTime;
+        transform.position = currentPosition;
+        previousPosition = currentPosition;
+    }
+}
